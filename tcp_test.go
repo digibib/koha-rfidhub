4607e8fdeac4668237c4879f853dd706e250a329
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-02-04 09:23:19 +0100

    fix test

--- tcp_test.go
+++ tcp_test.go
@@ -18,11 +18,21 @@ func TestTCPServer(t *testing.T) {
 	s.Expect(0, len(srv.connections))
 	go srv.run()
 
+	// Need to run websocket hub as well, otherwise tcpserver will block
+	// trying to broadcast messages
+	uiHub = NewHub()
+	go uiHub.run()
+
 	c, err := net.Dial("tcp", "localhost:6767")
 	s.ExpectNilFatal(err)
 	time.Sleep(time.Millisecond * 10)
 	s.Expect(1, len(srv.connections))
 
-	_, err = c.Write([]byte(`{"Cmd":"PING"}`))
+	_, err = c.Write([]byte("PING\n"))
 	s.ExpectNil(err)
+
+	err = c.Close()
+	s.ExpectNil(err)
+	time.Sleep(time.Millisecond * 10)
+	s.Expect(0, len(srv.connections))
 }
