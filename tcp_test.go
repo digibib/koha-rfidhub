227efd5e64475a72bef12e6256dbc714e0ca4062
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2014-02-06 09:39:27 +0100

    increase test coverage

--- tcp_test.go
+++ tcp_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"net"
 	"testing"
 	"time"
@@ -15,7 +16,7 @@ func TestTCPServer(t *testing.T) {
 		TCPPort: "6767",
 	}
 	srv := newTCPServer(cfg)
-	discardChan := make(chan UIMessage, 1)
+	discardChan := make(chan UIMessage, 10)
 	srv.broadcast = discardChan
 	go srv.run()
 	time.Sleep(time.Millisecond * 10)
@@ -23,8 +24,17 @@ func TestTCPServer(t *testing.T) {
 	c, err := net.Dial("tcp", "localhost:6767")
 	s.ExpectNilFatal(err)
 
+	time.Sleep(time.Millisecond * 10)
 	_, err = c.Write([]byte("PING\n"))
 	s.ExpectNil(err)
+
+	time.Sleep(time.Millisecond * 100)
+
+	srv.incoming <- []byte(`{ "ID":"` + c.LocalAddr().String() + `", "Msg": {"cmd":"HI-FROM-SERVER!"} }`)
+	r := bufio.NewReader(c)
+	msg, err := r.ReadString('\n')
+	s.ExpectNil(err)
+	s.Expect(`{"cmd":"HI-FROM-SERVER!"}`+"\n", msg)
 
 	err = c.Close()
 	s.ExpectNil(err)
